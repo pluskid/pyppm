12eb79a8380e2b5f143aa41bf2c1ce3c22329126
pluskid <pluskid@gmail.com>
2008-08-25 15:46:30 -0400

    Correct the encoder algorithm.

--- arith_codec.py
+++ arith_codec.py
@@ -3,14 +3,21 @@ class ArithCodec(object):
     Base class for ArithEncoder and ArithDecoder.
     """
 
-    # Precision of coding
-    CODE_VALUE_BITS = 27
+    # Number of bits for the range
+    TOP_VALUE_BITS = 28
+
+    # Number of bits to ignore when testing equality
+    APPROXIMATE_EQUALITY_PRECISION_BITS = 8
+
+    # If the difference between two numbers are less than
+    # this, they are considered equal
+    APPROXIMATE_EQUALITY_PRECISION = 1 << APPROXIMATE_EQUALITY_PRECISION_BITS
 
     # The maximum interval border
-    TOP_VALUE = (1 << CODE_VALUE_BITS) - 1
+    TOP_VALUE = 1 << TOP_VALUE_BITS
 
     # 1/4 of the largest range
-    FIRST_QUARTER = TOP_VALUE/4 + 1
+    FIRST_QUARTER = TOP_VALUE/4
 
     # 1/2 of the largest interval
     HALF = 2*FIRST_QUARTER
@@ -21,3 +28,12 @@ class ArithCodec(object):
     def __init__(self):
         self.low = 0
         self.high = self.TOP_VALUE
+
+    def approximate_ge(self, a, b):
+        """\
+        Test if a is arrpoximately greater than or equal to b.
+        """
+        diff = a-b
+        if diff >= -self.APPROXIMATE_EQUALITY_PRECISION:
+            return True
+        return False
--- arith_encoder.py
+++ arith_encoder.py
@@ -7,6 +7,16 @@ class ArithEncoder(ArithCodec):
         self.out = out
         self.bits_to_follow = 0
 
+    def close(self):
+        """\
+        Close the encoder and flush all buffered bits.
+        """
+        self.bits_to_follow += 1        # needs a final bit, not sure why
+        if self.low < self.FIRST_QUARTER:
+            self.write_false()
+        else:
+            self.write_true()
+
     def encode(self, low_count, high_count, total_count):
         """\
         Encode an interval.
@@ -15,24 +25,27 @@ class ArithEncoder(ArithCodec):
          - high_count: the cumulative count of symbol for this one and below
          - total_count: the cumulative count of all symbols
         """
-        rng = self.high-self.low+1
+        rng = self.high-self.low
 
-        self.high = self.low + rng*high_count/total_count - 1
+        self.high = self.low + rng*high_count/total_count
         self.low =  self.low + rng*low_count/total_count
 
         from sys import stdin
         while True:
-            print self.low, self.HALF, self.high
+            print '%10d, %10d, %10d' % (self.low, self.HALF, self.high)
+            print '%.4f, %.4f' % (float(self.low)/self.TOP_VALUE, float(self.high)/self.TOP_VALUE)
+            print
             
             if self.high < self.HALF:
                 self.write_false()
 
-            elif self.low >= self.HALF:
+            elif self.approximate_ge(self.low, self.HALF):
                 self.write_true()
                 self.low -= self.HALF
                 self.high -= self.HALF
                 
-            elif self.low >= self.FIRST_QUARTER and self.high < self.THIRD_QUARTER:
+            elif self.approximate_ge(self.low, self.FIRST_QUARTER) and \
+                     self.high < self.THIRD_QUARTER:
                 self.bits_to_follow += 1
                 
                 self.low -= self.FIRST_QUARTER
@@ -42,7 +55,8 @@ class ArithEncoder(ArithCodec):
                 break
 
             self.low = self.low << 1
-            self.high = (self.high << 1) + 1
+            self.high = self.high << 1
+
 
     def write_true(self):
         """\
